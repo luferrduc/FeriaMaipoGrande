43b665b56054b56097c22c544aa89cabee5edb00
luferrduc <lu.ferrando@duocuc.cl>
2022-09-05 21:26:31 -0300

    Creacion clase Transporte y Producto en Negocio

--- FeriaMaipoGrande/MainWindow.xaml.cs
+++ FeriaMaipoGrande/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FeriaMaipoGrande.Negocio;
 
 namespace FeriaMaipoGrande
 {
@@ -24,5 +25,8 @@ namespace FeriaMaipoGrande
         {
             InitializeComponent();
         }
+
+
+
     }
 }
