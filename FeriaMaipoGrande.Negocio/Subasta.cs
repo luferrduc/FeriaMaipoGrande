aaf57eb461c7989405dd0918f9aa358d1524ad49
Thiare Saez <thiareayleen@gmail.com>
2022-09-05 21:47:08 -0300

    Creacion atributos de clase subasta

---
+++ FeriaMaipoGrande.Negocio/Subasta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeriaMaipoGrande.Negocio
+{
+    class Subasta
+    {
+        private int IdSubasta { get; }
+        private String Ganador { get; set; }
+        private DateTime FechaIni { get; set; }
+        private DateTime FechaTer { get; set; }
+        private String Cargo { get; set; }
+        private int Precio { get; set; }
+        private String Estado { get; set; }
+
+    }
+}
