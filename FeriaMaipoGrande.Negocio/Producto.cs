2cac961db9a1f39e6959ba8b459138d7c995e1a2
luferrduc <lu.ferrando@duocuc.cl>
2022-09-05 21:45:21 -0300

    Revert "Revert "Creacion clase Transporte y Producto en Negocio""
    This reverts commit 624354f60f0c92337f687ed4963e4e4c75e269e0.

---
+++ FeriaMaipoGrande.Negocio/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeriaMaipoGrande.Negocio
+{
+    public class Producto
+    {
+        private int idProducto, precio;
+        private string nombre, calidad, observaciones;
+
+        public Producto(int idProducto, int precio, string nombre, string calidad, string observaciones)
+        {
+            this.IdProducto = idProducto;
+            this.Precio = precio;
+            this.Nombre = nombre;
+            this.Calidad = calidad;
+            this.Observaciones = observaciones;
+        }
+
+        public int IdProducto { get => idProducto; set => idProducto = value; }
+        public int Precio { get => precio; set => precio = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Calidad { get => calidad; set => calidad = value; }
+        public string Observaciones { get => observaciones; set => observaciones = value; }
+    }
+}
