94435aa1a8cc59b7ba4419ae3d187a9599dcde73
Thiare Saez <thiareayleen@gmail.com>
2022-09-05 23:11:28 -0300

    correcion atributos

--- FeriaMaipoGrande.Negocio/Subasta.cs
+++ FeriaMaipoGrande.Negocio/Subasta.cs
@@ -8,14 +8,23 @@ namespace FeriaMaipoGrande.Negocio
 {
     class Subasta
     {
-        private int IdSubasta { get; }
-        private String Ganador { get; set; }
-        private DateTime FechaIni { get; set; }
-        private DateTime FechaTer { get; set; }
-        private String Cargo { get; set; }
-        private int Precio { get; set; }
-        private String Estado { get; set; }
+        private int _idSubasta;
+        private String _ganador;
+        private DateTime _fechaIni; 
+        private DateTime _fechaTer;
+        private String _cargo;
+        private int  _precio; 
+        private String  _estado;
 
+
+        public int IdSubasta { get => _idSubasta;}
+        public String Ganador { get => _ganador; set => _ganador = value; }
+        public DateTime FechaIni { get => _fechaIni; set => _fechaIni = value; }
+        public DateTime FechaTer { get => _fechaTer; set => _fechaTer = value; }
+        public String Cargo { get => _cargo; set => _cargo = value; }
+        public int Precio { get => _precio;  set => _precio = value; }
+        public int Estado { get => _estado;  set =>  _estado = value; }
+         
         public String InformarGanador(String Ganador)
         {
             return "El ganador es " + Ganador;
