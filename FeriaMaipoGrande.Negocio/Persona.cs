800f0573db54d15dca00e90577db23eb154aa63c
JOSE REYES ALCAINO <jb.reyes@duocuc.cl>
2022-09-05 22:44:18 -0300

    Creación de clase padre persona.

--- FeriaMaipoGrande.Negocio/Persona.cs
+++ FeriaMaipoGrande.Negocio/Persona.cs
@@ -8,5 +8,33 @@ namespace FeriaMaipoGrande.Negocio
 {
     class Persona
     {
+        private int idPersona;
+        private String nombre, apellidoPaterno, apellidoMaterno, direccion, ciudad, pais;
+
+        //Constructor vacío
+        public Persona()
+        {
+        }
+
+        //Constructor con datos
+        public Persona(int idPersona, string nombre, string apellidoPaterno, string apellidoMaterno, string direccion, string ciudad, string pais)
+        {
+            this.IdPersona = idPersona;
+            this.Nombre = nombre;
+            this.apellidoPaterno = apellidoPaterno;
+            this.apellidoMaterno = apellidoMaterno;
+            this.direccion = direccion;
+            this.ciudad = ciudad;
+            this.pais = pais;
+        }
+
+        //Getters y setters
+        public int IdPersona { get => idPersona; set => idPersona = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string ApellidoPaterno { get => apellidoPaterno; set => apellidoPaterno = value; }
+        public string ApellidoMaterno { get => apellidoMaterno; set => apellidoMaterno = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Ciudad { get => ciudad; set => ciudad = value; }
+        public string Pais { get => pais; set => pais = value; }
     }
 }
