7ac275525809a7472ca167fc5bf23cc30daf71d7
JOSE REYES ALCAINO <jb.reyes@duocuc.cl>
2022-09-11 23:48:24 -0300

    ultimo cambio al consumo de la api y usuarios

--- FeriaMaipoGrande.Datos/UsuarioDAO.cs
+++ FeriaMaipoGrande.Datos/UsuarioDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,28 +7,67 @@ using System.Threading.Tasks;
 
 namespace FeriaMaipoGrande.Datos
 {
-    public class Usuario : PersonaDAO
+    public class UsuarioDAO //: PersonaDAO
     {
-        private int userID;
-        private string password, rol, email;
+        DBApi dbapi = new DBApi();
 
-        public Usuario(int idPersona, string nombre, string apellidoPaterno, string apellidoMaterno, string direccion, string ciudad, string pais, string numIdentificador) : base(idPersona, nombre, apellidoPaterno, apellidoMaterno, direccion, ciudad, pais, numIdentificador)
+        private int userID, rol;
+        private string username, password, email;
+
+        /*public Usuario(int idPersona, string username, string nombre, string apellidoPaterno, string apellidoMaterno, string direccion, string ciudad, string pais, string numIdentificador) : base(idPersona, nombre, apellidoPaterno, apellidoMaterno, direccion, ciudad, pais, numIdentificador)
         {
-        }
+        }*/
 
-        public Usuario(int userID, string password, string rol, string email)
+        public UsuarioDAO(int userID, string username, string password, int rol, string email)
         {
             UserID = userID;
+            Username = username;
             Password = password;
             Rol = rol;
             Email = email;
         }
-        public Usuario()
+        public UsuarioDAO()
         {
         }
-        public int UserID { get => userID; set => userID = value; }
+        [JsonProperty("password")]
         public string Password { get => password; set => password = value; }
-        public string Rol { get => rol; set => rol = value; }
+        [JsonProperty("nombre_usuario")]
+        public string Username { get => username; set => username = value; }
+        [JsonProperty("id_rol")]
+        public int Rol { get => rol; set => rol = value; }
+        [JsonProperty("id_persona")]
+        public int UserID { get => userID; set => userID = value; }
+        [JsonProperty("email")]
         public string Email { get => email; set => email = value; }
+        
+
+
+        public async void eliminarUsuarioDAO(string username)
+        {
+            await dbapi.DeleteUsuarioAsync("usuarios/", username);
+        }
+
+        public dynamic listarUsuariosDAO()
+        {
+            dynamic datos = dbapi.GetUsuarios("usuarios");
+
+            return datos;
+        }
+
+        public async void crearUsuarioDAO(UsuarioDAO usuario)
+        {
+            await dbapi.CrearUsuarioAsync("usuarios", usuario);
+        }
+
+        public async void actualizarUsuarioDAO(string username, UsuarioDAO usuario)
+        {
+            await dbapi.ActualizarUsuarioAsync("usuarios/", username, usuario);
+        }
+
+        public dynamic rolesUsuariosDAO()
+        {
+            dynamic roles = dbapi.GetRoles();
+            return roles;
+        }
     }
 }
